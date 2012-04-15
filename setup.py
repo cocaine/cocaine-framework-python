364b47489b87d8ff5b1c6a729b8de28fd3189899
Kobolog <me@kobology.ru>
2012-04-15 21:48:23 +0400

    pre-5: bindings

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup, Extension
 
 setup(
     name = "cocaine",
-    version = "0.7.2-2",
+    version = "0.7.3",
     description = "Cocaine Python Framework",
     long_description = "A simple framework to ease the development of Cocaine apps",
-    url = "https://github.com/kobolog/cocaine",
+    url = "https://github.com/cocaine/cocaine-framework-python",
     author = "Andrey Sibiryov",
     author_email = "me@kobology.ru",
     license = "BSD 2-Clause",
