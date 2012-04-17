3f65adba01527186f4fc2c22bb6960b2091e3e78
Kobolog <me@kobology.ru>
2012-04-17 14:46:27 +0400

    sane and equally useless changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 
 setup(
     name = "cocaine",
-    version = "0.7.3-b1",
+    version = "0.7.3-b3",
     description = "Cocaine Python Framework",
     long_description = "A simple framework to ease the development of Cocaine apps",
     url = "https://github.com/cocaine/cocaine-framework-python",
