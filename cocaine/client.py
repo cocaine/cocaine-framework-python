6f038cb6d989201455eaa63c1a47d2213be74690
Kobolog <me@kobology.ru>
2012-04-18 05:37:42 +0400

    more python-friendly client interface

--- cocaine/client.py
+++ cocaine/client.py
@@ -1,3 +1,41 @@
 # coding: utf-8
 
-from _client import Client
+import types
+import msgpack
+
+from _client import Client as ClientBase
+
+class Client(ClientBase):
+    """
+    Cocaine cloud balancer and scheduler.
+
+    To create a new client instance, specify the location of the client
+    configuration file.
+    """
+    
+    def __init__(self, config):
+        super(Client, self).__init__(config)
+
+    def send(self, path, message):
+        """
+        Sends a ``message`` to the cloud using the specified ``path``.
+
+        Valid ``path`` is a 'service/method'-like string, where 'service'
+        is a configured service alias, and 'handle' is the actual app method
+        name.
+
+        If ``message`` does support buffer protocol, it will be sent as-is,
+        otherwise an attempt will be made to serialize it with MessagePack,
+        sending the resulting bytestream instead.
+        """
+
+        try:
+            service, handle = path.split('/')
+        except ValueError:
+            raise ValueError("Malformed message path")
+
+        if not isinstance(message, types.StringTypes):
+            message = msgpack.packb(message)
+
+        return super(Client, self).send(service, handle, message)
+
