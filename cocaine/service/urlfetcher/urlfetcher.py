0c060a5c37720d0081a3c6ffaa9c1e17563eabc1
noxiouz <noxiouz@yandex.ru>
2013-03-27 01:23:50 +0400

    Make urlfetcher's protocol

--- cocaine/service/urlfetcher/urlfetcher.py
+++ cocaine/service/urlfetcher/urlfetcher.py
@@ -1,14 +1,24 @@
 from cocaine.service.services import _BaseService
 
+from cocaine.asio.message import PROTOCOL_LIST
+from cocaine.asio.message import Message
+
 class Urlfetcher(_BaseService):
+    """ Port 12502
+    [0, 256, [["http://company.yandex.ru", [], True]]]
+    [0, 257, [["http://google.com", [], True]]]
+    """
 
     def __init__(self):
+        """ _counter - unique id generator
+        _subscribers - mapping ids to callback
+        """
         super(Urlfetcher, self).__init__(('localhost', 12502))
         self._subscribers = dict()
         self._counter = 0
 
     def _fetch(self, url, counter):
-        self.m_w_stream.write([0, counter, [["http://company.yandex.ru", [], True]]])
+        self.m_w_stream.write([0, counter, [[url, [], True]]])
 
     def get(self, url):
         def wrapper(clbk):
@@ -17,11 +27,12 @@ class Urlfetcher(_BaseService):
             self._fetch(url, self._counter)
         return wrapper
 
-    def on_message(self, *args):
-        try:
-            num =  args[0][1]
-            data = args[0][2]
-            self._subscribers[num](data)
-            self._subscribers.pop(num, None)
-        except Exception as err:
-            print "ERROR", str(err)
+    def on_message(self, args):
+        msg = Message.initialize(args)
+        if msg is None:
+            print "Drop invalid message"
+            return
+        if msg.id == PROTOCOL_LIST.index("rpc::chunk"):
+            self._subscribers[msg.session](msg.data)
+        elif msg.id == PROTOCOL_LIST.index("rpc::choke"):
+            self._subscribers.pop(msg.session, None)
