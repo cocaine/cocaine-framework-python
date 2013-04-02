39e83e1e181c8224acd07d688d8405ab28198978
noxiouz <noxiouz@yandex.ru>
2013-04-03 03:26:16 +0400

    Fresh example

--- examples/async_service_and_dealer.py
+++ examples/async_service_and_dealer.py
@@ -3,18 +3,9 @@
 from hashlib import sha512
 
 from cocaine.worker import Worker
-from cocaine.decorators import *
-from cocaine.service.logger import Log
-from cocaine.service.logger import Logger
+from cocaine.logger import Logger
 from cocaine.service.services import Service
 from cocaine.exceptions import *
-
-
-#l = Log()
-#l.info("INFO")
-#l.debug("DEBUG")
-#l.error("ERROR")
-#l.warn("WARN")
 
 L = Logger()
 
@@ -22,33 +13,32 @@ urlfetcher_service = Service("urlfetch")
 storage_service = Service("storage")
 
 def example(request, response):
-    print "INITIALIZE FUNCTION"
+    L.info("INITIALIZE FUNCTION")
     try:
         ls = yield storage_service.list()
-        print "From storage: %s" % ls
+        L.info("From storage: %s" % ls)
     except ServiceError as err:
-        print "S: %s" % err
+        L.error("S: %s" % err)
         ls = yield storage_service.list("manifests")
-        print "From storage: %s" % ls
-    print "Now yield"
-    yield
-    print "After yield"
+        L.info("From storage: %s" % ls)
 
-    print "Request www.ya.ru"
+    L.info("Now yield")
+    yield
+    L.info("After yield")
+
+    L.info("Request www.ya.ru")
     webpage = yield urlfetcher_service.get("www.ya.ru",{}, True)
-    #l.info(sha512(webpage).hexdigest())
 
     chunk_from_cloud = yield request.read()
-    print "from dealer:", chunk_from_cloud
+    L.info("from dealer: %s" % chunk_from_cloud)
     try:
         chunk_from_cloud = yield request.read()
-        print "from dealer:", chunk_from_cloud
+        L.info("from dealer: %s" % chunk_from_cloud)
     except RequestError as err:
-        print "R: %s" % err
+        L.error("R: %s" % err)
 
-    print "Request www.google.ru"
+    L.info("Request www.google.ru")
     webpage = yield urlfetcher_service.get("www.google.ru",{}, True)
-    #l.info(sha512(webpage).hexdigest())
 
     response.write("EXAMPLE")
     response.close()
@@ -57,20 +47,20 @@ def nodejs(request, response):
     """ Nodejs style """
     def on_url(chunk):
         def on_request(chunk):
-            print "Request chunk: %s" % chunk
+            L.info("Request chunk: %s" % chunk)
             response.write("NODEJS")
             response.close()
-        print "Webpage hash: %s" % sha512(chunk).hexdigest()
+        L.info("Webpage hash: %s" % sha512(chunk).hexdigest())
         future = request.read()
         future(on_request)
 
     def errorback(error):
-        print "errorback"
-        print error
+        L.info("errorback")
+        L.info(error)
         future = urlfetcher_service.get("www.ya.ru",{}, True)
         future(on_url)
 
-    print "INITIALIZE FUNCTION"
+    L.info("INITIALIZE FUNCTION")
     future = urlfetcher_service.get()
     future(on_url, errorback)
 
