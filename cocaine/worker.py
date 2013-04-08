0ba819e1043ae44363e59b5e51154c8f4d62dd53
noxiouz <noxiouz@yandex.ru>
2013-04-08 14:00:57 +0400

    Add optional init_args

--- cocaine/worker.py
+++ cocaine/worker.py
@@ -41,7 +41,7 @@ from cocaine.exceptions import RequestError
 class Worker(object):
 
     def __init__(self, init_args=sys.argv):
-        print "AAAAA"
+        print init_args
         self._logger = Logger()
         self._init_endpoint(init_args)
 
