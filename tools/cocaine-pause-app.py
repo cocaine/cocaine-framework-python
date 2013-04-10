7488d9ee54f0ae66c17afdf26db68e193e5125e5
noxiouz <noxiouz@yandex.ru>
2013-04-11 03:03:59 +0400

    Rename tools to *.py

--- tools/cocaine-pause-app.py
+++ tools/cocaine-pause-app.py
@@ -1,4 +1,23 @@
 #! /usr/bin/env python
+#
+#    Copyright (c) 2011-2013 Anton Tyurin <noxiouz@yandex.ru>
+#    Copyright (c) 2011-2013 Other contributors as noted in the AUTHORS file.
+#
+#    This file is part of Cocaine.
+#
+#    Cocaine is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Cocaine is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>. 
+#
 
 from pprint import pprint
 from optparse import OptionParser
