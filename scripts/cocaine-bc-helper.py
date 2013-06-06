0203f0163d94b7674cf89c0a419291caaaa63c24
Evgeny Safronov <division494@gmail.com>
2013-06-06 14:52:22 +0400

    Bash completion bug has been fixed
     - suddenly, bash parses arguments like --host=localhost to three arguments: [--host, =, localhost]. This behaviour has been taken into consideration

--- scripts/cocaine-bc-helper.py
+++ scripts/cocaine-bc-helper.py
@@ -2,15 +2,19 @@
 __author__ = 'EvgenySafronov <division494@gmail.com>'
 
 
-def getOption(optionName, default):
-    for name in [optionName, optionName + '=']:
-        if name in sys.argv:
-            index = sys.argv.index(name)
-            return sys.argv[index + 1] if index + 1 < len(sys.argv) else default
-        for arg in sys.argv:
-            if arg.startswith(name):
-                return arg[len(name) + 1:] or default
-    return default
+def getOption(name, default):
+    value = default
+    if name in sys.argv:
+        index = sys.argv.index(name)
+        if index < len(sys.argv) - 1:
+            value = sys.argv[index + 1]
+            if value == '=' and index + 1 < len(sys.argv) - 2:
+                value = sys.argv[index + 2]
+    elif name + '=' in sys.argv:
+        index = sys.argv.index(name + '=')
+        if index < len(sys.argv) - 1:
+            value = sys.argv[index + 1]
+    return value
 
 
 if __name__ == '__main__':
@@ -31,7 +35,7 @@ if __name__ == '__main__':
         config = {
             'locateItem': getOption('--locator_type', 'app'),
             'host': getOption('--host', 'localhost'),
-            'port': getOption('--port', 10053)
+            'port': getOption('--port', '10053')
         }
 
         def locateApps():
@@ -41,10 +45,10 @@ if __name__ == '__main__':
             print(' '.join(apps.get()))
             loop.stop()
 
-        storage = Service('storage', config['host'], config['port'])
+        storage = Service('storage', config['host'], int(config['port']))
         ChainFactory().then(locateApps).then(printResult).run()
         loop = IOLoop.instance()
-        loop.add_timeout(time() + 0.5, lambda: loop.stop())
+        loop.add_timeout(time() + 0.25, lambda: loop.stop())
         loop.start()
     except Exception as err:
         # Hidden log feature :)
