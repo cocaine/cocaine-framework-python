c6f3f4dd497ff78cdb7c6c611f3328036996f9d3
Anton Tyurin <noxiouz@yandex.ru>
2013-06-11 11:50:51 +0400

    Add python example for service

---
+++ examples/dummy_service.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from cocaine.worker import Worker
+from cocaine.services import Service
+
+storage = Service("storage")
+
+def write_dummy(request, response):
+    req = yield request.read()
+    yield storage.write("dummy-namespace", "dummy-key",\
+                            req, ["dummy-tag"])
+    dummy = yield storage.read("dummy-namespace", "dummy-key")
+    response.write(dummy)
+    response.close()
+
+W = Worker()
+W.run({"write_dummy" : write_dummy})
