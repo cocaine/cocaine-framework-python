7aac18fe30c706aa0a851b8707277fb101bc558e
Evgeny Safronov <division494@gmail.com>
2013-06-18 16:07:03 +0400

    Internal examples revamped

--- examples/internal/chaining.py
+++ examples/internal/chaining.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-import logging
+import functools
 from cocaine.futures.chain import ChainFactory
 import time
 from tornado.ioloop import IOLoop
@@ -8,20 +8,30 @@ from cocaine.services import Service
 __author__ = 'EvgenySafronov <division494@gmail.com>'
 
 
-def func():
+def trackable(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.called += 1
+        func(*args, **kwargs)
+    wrapper.called = 0
+    return wrapper
+
+
+def invokeNodeInfo():
     c = node.info()
     c.then(handle).run()
 
 
+@trackable
 def handle(result):
     print "Handle", result.obj
 
 
 def func2():
-    print 'AA'
     info1 = yield node.info()
     info2 = yield node.info()
     print(info1, info2)
+
 
 def func3(args):
     print "Start"
@@ -31,17 +41,15 @@ def func3(args):
 
 
 if __name__ == '__main__':
-    log = logging.getLogger(__name__)
-    #chainLog = logging.getLogger('cocaine.futures.chain')
-    log.setLevel(logging.DEBUG)
-    #chainLog.setLevel(logging.DEBUG)
-
-
     node = Service('node')
-    func()
+    invokeNodeInfo()
+
+    def shutdown():
+        loop.stop()
+        assert handle.called == 1
 
     ChainFactory([func2, func3]).run()
     loop = IOLoop.instance()
-    loop.add_timeout(time.time() + 2, lambda: loop.stop() )
+    loop.add_timeout(time.time() + 0.2, shutdown)
     loop.start()
 
