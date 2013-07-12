02a9a64451ddbfee165c65e7a7c2de44cd9b8b8e
Evgeny Safronov <division494@gmail.com>
2013-07-12 16:04:02 +0400

    Documentation for cocaine-tools crashlogs methods has been added

--- cocaine/tools/dispatcher.py
+++ cocaine/tools/dispatcher.py
@@ -524,7 +524,7 @@ def runlist_add_app(locator,
     :param app: application name.
     :param profile: suggested profile name.
 
-    >>>cocaine-tool runlist add-app --name default --app Echo --profile EchoProfile
+    >>> cocaine-tool runlist add-app --name default --app Echo --profile EchoProfile
     {
         "status": "Success",
         "added": {
@@ -544,8 +544,18 @@ def runlist_add_app(locator,
 @crashlogDispatcher.command(name='list')
 def crashlog_list(locator,
                   name=('n', '', 'name')):
-    """
-    Shows crashlogs list for application
+    """Show crashlogs list for application.
+
+    Prints crashlog list in timestamp - uuid format.
+
+    :param name: application name.
+
+    >>> cocaine-tool crashlog list --name Echo
+    Currently available crashlogs for application 'Echo'
+    1372165800114964 Tue Jun 25 17:10:00 2013 2d92aa19-535d-4aa3-9c68-7aa32f9967df
+    1372166090866950 Tue Jun 25 17:14:50 2013 e27b2ccc-64a6-4958-a9b4-f2abac974e4a
+    1372166371522675 Tue Jun 25 17:19:31 2013 762f2fb8-8d8c-4b1d-ab79-14cdb6332ecb
+    1372166822795587 Tue Jun 25 17:27:02 2013 1fd3ca03-3402-4279-8b2b-1e40ff92f4a7
     """
     locator.storageExecutor.executeAction('crashlog:list', **{
         'name': name
@@ -556,8 +566,25 @@ def crashlog_list(locator,
 def crashlog_view(locator,
                   name=('n', '', 'name'),
                   timestamp=('t', '', 'timestamp')):
-    """
-    Shows crashlog for application with specified timestamp
+    """Show crashlog for application with specified timestamp.
+
+    :param name: application name.
+    :param timestamp: desired timestamp - time_t format.
+
+    >>> cocaine-tool crashlog view --name Echo --timestamp 1372165800114964
+    Crashlog:
+      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/ioloop.py", line 672, in start
+        self._handlers[fd](fd, events)
+      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/stack_context.py", line 331, in wrapped
+        raise_exc_info(exc)
+      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/stack_context.py", line 302, in wrapped
+        ret = fn(*args, **kwargs)
+      File "build/bdist.macosx-10.8-intel/egg/cocaine/asio/ev.py", line 93, in proxy
+        self._callbacks[(fd, self.WRITE)]()
+      File "build/bdist.macosx-10.8-intel/egg/cocaine/asio/stream.py", line 128, in _on_event
+        sent = self.pipe.write(buffer(current, self.tx_offset))
+    TypeError: an integer is required
+    ERROR:tornado.application:Exception in I/O handler for fd 11
     """
     locator.storageExecutor.executeAction('crashlog:view', **{
         'name': name,
@@ -569,8 +596,13 @@ def crashlog_view(locator,
 def crashlog_remove(locator,
                     name=('n', '', 'name'),
                     timestamp=('t', '', 'timestamp')):
-    """
-    Removes crashlog for application with specified timestamp
+    """Remove crashlog for application with specified timestamp from the storage.
+
+    :param name: application name.
+    :param timestamp: desired timestamp - time_t format.
+
+    >>> cocaine-tool crashlog remove --name Echo --timestamp 1372165800114964
+    Crashlog for app "Echo" have been removed
     """
     locator.storageExecutor.executeAction('crashlog:remove', **{
         'name': name,
@@ -581,8 +613,12 @@ def crashlog_remove(locator,
 @crashlogDispatcher.command(name='removeall')
 def crashlog_removeall(locator,
                        name=('n', '', 'name')):
-    """
-    Removes all crashlogs for application
+    """Remove all crashlogs for application from the storage.
+
+    :param name: application name.
+
+    >>> cocaine-tool crashlog removeall --name Echo
+    Crashlogs for app "Echo" have been removed
     """
     locator.storageExecutor.executeAction('crashlog:removeall', **{
         'name': name,
