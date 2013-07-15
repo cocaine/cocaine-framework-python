c560933d0b8a12e53709aa10a476d1e5c04b153b
Evgeny Safronov <division494@gmail.com>
2013-07-15 18:29:45 +0400

    Documentation has been reorganized.

--- cocaine/tools/dispatcher.py
+++ cocaine/tools/dispatcher.py
@@ -59,41 +59,9 @@ def info(locator):
     """Show information about cocaine runtime
 
     Return json-like string with information about cocaine-runtime.
-
-    >>> cocaine-tools info
-    {
-        "uptime": 738,
-        "identity": "dhcp-666-66-wifi.yandex.net"
-    }
-
-    If some applications is running, its information will be displayed too.
-
-    >>> cocaine-tools info
-    {
-        "uptime": 738,
-        "apps": {
-            "Echo": {
-                "load-median": 0,
-                "profile": "EchoProfile",
-                "sessions": {
-                    "pending": 0
-                },
-                "queue": {
-                    "depth": 0,
-                    "capacity": 100
-                },
-                "state": "running",
-                "slaves": {
-                    "active": 0,
-                    "idle": 0,
-                    "capacity": 4
-                }
-            }
-        },
-        "identity": "dhcp-666-66-wifi.yandex.net"
-    }
     """
     locator.nodeExecutor.executeAction('info', **{})
+
 
 @d.command()
 def call(locator,
@@ -106,31 +74,6 @@ def call(locator,
 
     Method arguments should be passed in double quotes as they would be written in Python.
     If no method provided, service API will be printed.
-
-    *Request service API*:
-
-    >>> cocaine-tool call node
-    API of service "node": [
-        "start_app",
-        "pause_app",
-        "info"
-    ]
-
-    *Invoke `info` method from service `node`*:
-
-    >>> cocaine-tool call node info
-    {'uptime': 1855, 'identity': 'dhcp-666-66-wifi.yandex.net'}
-
-    *Specifying locator endpoint*
-
-    >>> cocaine-tool call node info --host localhost --port 10052
-    LocatorResolveError: Unable to resolve API for service node at localhost:10052, because [Errno 61] Connection
-    refused
-
-    *Passing complex method arguments*
-
-    >>> cocaine-tool call storage read "'apps', 'Echo'"
-    [Lot of binary data]
     """
     command = service + '.' + method + '(' + args + ')'
     locator.nodeExecutor.executeAction('call', **{
@@ -139,14 +82,7 @@ def call(locator,
 
 @appDispatcher.command(name='list')
 def app_list(locator):
-    """Show installed applications list.
-
-    >>> cocaine-tools app list
-    [
-        "app1",
-        "app2"
-    ]
-    """
+    """Show installed applications list."""
     locator.storageExecutor.executeAction('app:list', **{})
 
 
@@ -156,13 +92,6 @@ def app_view(locator,
     """Show manifest context for application.
 
     If application is not uploaded, an error will be displayed.
-
-    :param name: application name.
-
-    >>> cocaine-tool app view --name Echo
-    {
-        "slave": "/home/satan/echo/echo.py"
-    }
     """
     locator.storageExecutor.executeAction('app:view', **{
         'name': name,
@@ -174,15 +103,7 @@ def app_upload(locator,
                name=('n', '', 'application name'),
                manifest=('', '', 'manifest file name'),
                package=('', '', 'location of the app source package')):
-    """Upload application into the storage
-
-    :param name: application name.
-    :param manifest: path to application manifest json file.
-    :param package: path to application archive.
-
-    >>> cocaine-tool app upload --name echo --manifest ~/echo/manifest.json --package ~/echo/echo.tar.gz
-    Application echo has been successfully uploaded
-    """
+    """Upload application into the storage"""
     locator.storageExecutor.executeAction('app:upload', **{
         'name': name,
         'manifest': manifest,
@@ -198,15 +119,6 @@ def app_upload2(locator,
 
     Application directory must contain valid manifest file.
     You can specify application name. By default, directory name is treated as application name.
-
-    :param path: path to the application root.
-    :param name: application name. If it is not specified, application will be named as its directory name.
-
-    >>> cocaine-tool app upload2 ~/echo
-    Application echo has been successfully uploaded
-
-    >>> cocaine-tool app upload2 ~/echo TheEchoApp
-    Application TheEchoApp has been successfully uploaded
     """
     locator.storageExecutor.executeAction('app:upload2', **{
         'path': path,
@@ -220,11 +132,6 @@ def app_remove(locator,
     """Remove application from storage.
 
     No error messages will display if specified application is not uploaded.
-
-    :param name: application name.
-
-    >>> cocaine-tool app remove --name echo
-    The app "echo" has been successfully removed
     """
     locator.storageExecutor.executeAction('app:remove', **{
         'name': name
@@ -238,21 +145,6 @@ def app_start(locator,
     """Start application with specified profile.
 
     Does nothing if application is already running.
-
-    :param name: application name.
-    :param profile: desired profile.
-
-    >>> cocaine-tool app start --name Echo --profile EchoDefault
-    {
-        "Echo": "the app has been started"
-    }
-
-    *If application is already running*
-
-    >>> cocaine-tool app start --name Echo --profile EchoDefault
-    {
-        "Echo": "the app is already running"
-    }
     """
     locator.nodeExecutor.executeAction('app:start', **{
         'name': name,
@@ -266,20 +158,6 @@ def app_pause(locator,
     """Stop application.
 
     This command is alias for ```cocaine-tool app stop```.
-
-    :param name: application name.
-
-    >>> cocaine app pause --name Echo
-    {
-        "Echo": "the app has been stopped"
-    }
-
-    *For non running application*
-
-    >>> cocaine app pause --name Echo
-    {
-        "Echo": "the app is not running"
-    }
     """
     locator.nodeExecutor.executeAction('app:pause', **{
         'name': name
@@ -289,22 +167,7 @@ def app_pause(locator,
 @appDispatcher.command(name='stop')
 def app_stop(locator,
              name=('n', '', 'application name')):
-    """Stop application.
-
-    :param name: application name.
-
-    >>> cocaine app stop --name Echo
-    {
-        "Echo": "the app has been stopped"
-    }
-
-    *For non running application*
-
-    >>> cocaine app stop --name Echo
-    {
-        "Echo": "the app is not running"
-    }
-    """
+    """Stop application."""
     locator.nodeExecutor.executeAction('app:stop', **{
         'name': name
     })
@@ -319,50 +182,6 @@ def app_restart(locator,
     Executes ```cocaine-tool app pause``` and ```cocaine-tool app start``` sequentially.
 
     It can be used to quickly change application profile.
-
-    :param name: application name.
-    :param profile: desired profile. If no profile specified, application will be restarted with the current profile.
-
-    *Usual case*
-
-    >>> cocaine-tool app restart --name Echo
-    [
-        {
-            "Echo": "the app has been stopped"
-        },
-        {
-            "Echo": "the app has been started"
-        }
-    ]
-
-    *If application was not run and no profile name provided*
-
-    >>> cocaine-tool app restart --name Echo
-    Error occurred: Application "Echo" is not running and profile not specified
-
-    *But if we specify profile name*
-
-    >>> cocaine-tool app restart --name Echo --profile EchoProfile
-    [
-        {
-            "Echo": "the app is not running"
-        },
-        {
-            "Echo": "the app has been started"
-        }
-    ]
-
-    *In case wrong profile just stops application*
-
-    >>> cocaine-tool app restart --name Echo --profile EchoProf
-    [
-        {
-            "Echo": "the app has been stopped"
-        },
-        {
-            "Echo": "object 'EchoProf' has not been found in 'profiles'"
-        }
-    ]
     """
     locator.nodeExecutor.executeAction('app:restart', **{
         'name': name,
@@ -373,15 +192,7 @@ def app_restart(locator,
 @appDispatcher.command()
 def check(locator,
           name=('n', '', 'application name')):
-    """Checks application status.
-
-    :param name: application name.
-
-    >>> cocaine-tool app check --name Echo
-    {
-        "Echo": "stopped or missing"
-    }
-    """
+    """Checks application status."""
     locator.nodeExecutor.executeAction('app:check', **{
         'name': name
     })
@@ -389,28 +200,14 @@ def check(locator,
 
 @profileDispatcher.command(name='list')
 def profile_list(locator):
-    """Show installed profiles.
-
-    >>> cocaine-tool profile list
-    [
-        "EchoProfile"
-    ]
-    """
+    """Show installed profiles."""
     locator.storageExecutor.executeAction('profile:list', **{})
 
 
 @profileDispatcher.command(name='view')
 def profile_view(locator,
                  name=('n', '', 'profile name')):
-    """Show profile configuration context.
-
-    :param name: profile name
-
-    >>> cocaine-tool profile view --name EchoProfile
-    {
-        "pool-limit": 4
-    }
-    """
+    """Show profile configuration context."""
     locator.storageExecutor.executeAction('profile:view', **{
         'name': name
     })
@@ -420,14 +217,7 @@ def profile_view(locator,
 def profile_upload(locator,
                    name=('n', '', 'profile name'),
                    profile=('', '', 'path to profile file')):
-    """Upload profile into the storage.
-
-    :param name: profile name.
-    :param profile: path to the profile json file.
-
-    >>> cocaine-tool profile upload --name EchoProfile --profile ../examples/echo/profile.json
-    The profile "EchoProfile" has been successfully uploaded
-    """
+    """Upload profile into the storage."""
     locator.storageExecutor.executeAction('profile:upload', **{
         'name': name,
         'manifest': profile
@@ -435,15 +225,9 @@ def profile_upload(locator,
 
 
 @profileDispatcher.command(name='remove')
-def profile_upload(locator,
+def profile_remove(locator,
                    name=('n', '', 'profile name')):
-    """Remove profile from the storage.
-
-    :param name: profile name.
-
-    >>> cocaine-tool profile remove --name EchoProfile
-    The profile "EchoProfile" has been successfully removed
-    """
+    """Remove profile from the storage."""
     locator.storageExecutor.executeAction('profile:remove', **{
         'name': name
     })
@@ -451,28 +235,14 @@ def profile_upload(locator,
 
 @runlistDispatcher.command(name='list')
 def runlist_list(locator):
-    """Show uploaded runlists.
-
-    >>> cocaine-tool runlist list
-    [
-        "default"
-    ]
-    """
+    """Show uploaded runlists."""
     locator.storageExecutor.executeAction('runlist:list', **{})
 
 
 @runlistDispatcher.command(name='view')
 def runlist_view(locator,
                  name=('n', '', 'name')):
-    """Show configuration context for runlist.
-
-    :param name: runlist name.
-
-    >>> ./cocaine-tool runlist view --name default
-    {
-        "Echo": "EchoProfile"
-    }
-    """
+    """Show configuration context for runlist."""
     locator.storageExecutor.executeAction('runlist:view', **{
         'name': name
     })
@@ -482,14 +252,7 @@ def runlist_view(locator,
 def runlist_upload(locator,
                    name=('n', '', 'name'),
                    runlist=('', '', 'path to the runlist configuration json file')):
-    """Upload runlist with context into the storage.
-
-    :param name: runlist name.
-    :param runlist: path to the runlist configuration json file.
-
-    >>> ./cocaine-tool runlist upload --name default --runlist ../examples/echo/runlsit.json
-    The runlist "default" has been successfully uploaded
-    """
+    """Upload runlist with context into the storage."""
     locator.storageExecutor.executeAction('runlist:upload', **{
         'name': name,
         'manifest': runlist
@@ -499,13 +262,7 @@ def runlist_upload(locator,
 @runlistDispatcher.command(name='create')
 def runlist_create(locator,
                    name=('n', '', 'name')):
-    """Create runlist and upload it into the storage.
-
-    :param name: runlist name.
-
-    >>> ./cocaine-tool runlist create --name default
-    The runlist "default" has been successfully created
-    """
+    """Create runlist and upload it into the storage."""
     locator.storageExecutor.executeAction('runlist:create', **{
         'name': name
     })
@@ -514,13 +271,7 @@ def runlist_create(locator,
 @runlistDispatcher.command(name='remove')
 def runlist_remove(locator,
                    name=('n', '', 'name')):
-    """Remove runlist from the storage.
-
-    :param name: runlist name.
-
-    >>> cocaine-tool runlist remove --name default
-    The runlist "default" has been successfully removed
-    """
+    """Remove runlist from the storage."""
     locator.storageExecutor.executeAction('runlist:remove', **{
         'name': name
     })
@@ -534,20 +285,6 @@ def runlist_add_app(locator,
     """Add specified application with profile to the runlist.
 
     Existence of application or profile is not checked.
-
-    :param name: runlist name.
-    :param app: application name.
-    :param profile: suggested profile name.
-
-    >>> cocaine-tool runlist add-app --name default --app Echo --profile EchoProfile
-    {
-        "status": "Success",
-        "added": {
-            "profile": "EchoProfile",
-            "app": "Echo"
-        },
-        "runlist": "default"
-    }
     """
     locator.storageExecutor.executeAction('runlist:add-app', **{
         'name': name,
@@ -562,15 +299,6 @@ def crashlog_list(locator,
     """Show crashlogs list for application.
 
     Prints crashlog list in timestamp - uuid format.
-
-    :param name: application name.
-
-    >>> cocaine-tool crashlog list --name Echo
-    Currently available crashlogs for application 'Echo'
-    1372165800114964 Tue Jun 25 17:10:00 2013 2d92aa19-535d-4aa3-9c68-7aa32f9967df
-    1372166090866950 Tue Jun 25 17:14:50 2013 e27b2ccc-64a6-4958-a9b4-f2abac974e4a
-    1372166371522675 Tue Jun 25 17:19:31 2013 762f2fb8-8d8c-4b1d-ab79-14cdb6332ecb
-    1372166822795587 Tue Jun 25 17:27:02 2013 1fd3ca03-3402-4279-8b2b-1e40ff92f4a7
     """
     locator.storageExecutor.executeAction('crashlog:list', **{
         'name': name
@@ -581,26 +309,7 @@ def crashlog_list(locator,
 def crashlog_view(locator,
                   name=('n', '', 'name'),
                   timestamp=('t', '', 'timestamp')):
-    """Show crashlog for application with specified timestamp.
-
-    :param name: application name.
-    :param timestamp: desired timestamp - time_t format.
-
-    >>> cocaine-tool crashlog view --name Echo --timestamp 1372165800114964
-    Crashlog:
-      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/ioloop.py", line 672, in start
-        self._handlers[fd](fd, events)
-      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/stack_context.py", line 331, in wrapped
-        raise_exc_info(exc)
-      File "/Library/Python/2.7/site-packages/tornado-3.1-py2.7.egg/tornado/stack_context.py", line 302, in wrapped
-        ret = fn(*args, **kwargs)
-      File "build/bdist.macosx-10.8-intel/egg/cocaine/asio/ev.py", line 93, in proxy
-        self._callbacks[(fd, self.WRITE)]()
-      File "build/bdist.macosx-10.8-intel/egg/cocaine/asio/stream.py", line 128, in _on_event
-        sent = self.pipe.write(buffer(current, self.tx_offset))
-    TypeError: an integer is required
-    ERROR:tornado.application:Exception in I/O handler for fd 11
-    """
+    """Show crashlog for application with specified timestamp."""
     locator.storageExecutor.executeAction('crashlog:view', **{
         'name': name,
         'manifest': timestamp
@@ -611,14 +320,7 @@ def crashlog_view(locator,
 def crashlog_remove(locator,
                     name=('n', '', 'name'),
                     timestamp=('t', '', 'timestamp')):
-    """Remove crashlog for application with specified timestamp from the storage.
-
-    :param name: application name.
-    :param timestamp: desired timestamp - time_t format.
-
-    >>> cocaine-tool crashlog remove --name Echo --timestamp 1372165800114964
-    Crashlog for app "Echo" have been removed
-    """
+    """Remove crashlog for application with specified timestamp from the storage."""
     locator.storageExecutor.executeAction('crashlog:remove', **{
         'name': name,
         'manifest': timestamp
@@ -628,13 +330,7 @@ def crashlog_remove(locator,
 @crashlogDispatcher.command(name='removeall')
 def crashlog_removeall(locator,
                        name=('n', '', 'name')):
-    """Remove all crashlogs for application from the storage.
-
-    :param name: application name.
-
-    >>> cocaine-tool crashlog removeall --name Echo
-    Crashlogs for app "Echo" have been removed
-    """
+    """Remove all crashlogs for application from the storage."""
     locator.storageExecutor.executeAction('crashlog:removeall', **{
         'name': name,
         'manifest': None
