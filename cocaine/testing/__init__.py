bf1e18b3fe624e17c44dbe52a983a91ca4e52fe4
Evgeny Safronov <division494@gmail.com>
2013-07-31 18:03:32 +0400

    Primitive asynchronous testing support

--- cocaine/testing/__init__.py
+++ cocaine/testing/__init__.py
@@ -1,1 +1,12 @@
+import functools
+from cocaine.futures.chain import Chain
+
 __author__ = 'Evgeny Safronov <division494@gmail.com>'
+
+
+def gen_test(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        Chain([lambda: func(self, *args, **kwargs), lambda result: self.ioLoop.stop()])
+        self.ioLoop.start()
+    return wrapper
