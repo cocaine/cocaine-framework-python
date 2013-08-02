feb4bda75c6967447b25237dc8ce408db04aff97
Evgeny Safronov <division494@gmail.com>
2013-08-03 03:08:25 +0400

    Garbage collection

--- TODO.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'EvgenySafronov <division494@gmail.com>'
-
-
-APP_UPLOAD_FROM_GIT = '''
-API: cocaine-tool app upload <URL>
-CVS: git, svn, hg, [...]
-LANGUAGES: C++ [just so], Python [create venv], [...]
-REQUIREMENTS: For every languages there are own requirements
-'''
