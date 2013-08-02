62484574f8aabcab781072b2c11adb62fd22a5e6
Evgeny Safronov <division494@gmail.com>
2013-08-02 15:08:42 +0400

    Code refactoring

--- cocaine/asio/ng/service.py
+++ cocaine/asio/ng/service.py
@@ -18,13 +18,35 @@ __author__ = 'Evgeny Safronov <division494@gmail.com>'
 log = logging.getLogger(__name__)
 
 
+class strategy:
+    @classmethod
+    def init(cls, func, isBlocking):
+        return strategy.sync(func) if isBlocking else strategy.async(func)
+
+    @classmethod
+    def sync(cls, func):
+        def wrapper(*args, **kwargs):
+            coroutine = func(*args, **kwargs)
+            chunk = None
+            while True:
+                try:
+                    chunk = coroutine.send(chunk)
+                except StopIteration:
+                    break
+        return wrapper
+
+    @classmethod
+    def async(cls, func):
+        return chain.source(func)
+
+
 RESOLVE_METHOD_ID = 0
 
 
 class AbstractService(object):
-    def __init__(self, name, blocking):
+    def __init__(self, name, isBlocking):
         self.name = name
-        self.blocking = blocking
+        self.isBlocking = isBlocking
 
         self._pipe = None
         self._ioLoop = None
@@ -37,20 +59,16 @@ class AbstractService(object):
         self._subscribers = {}
         self._session = 0
 
+        self._connect = strategy.init(self._connect, isBlocking)
+
     def isConnected(self):
         return self._pipe is not None and self._pipe.isConnected()
 
-    def _connect(self, host, port):
-        if self.blocking:
-            return self._connectSynchronously(host, port)
-        else:
-            return self._connectAsynchronously(host, port)
-
-    @chain.source
-    def _connectAsynchronously(self, host, port, timeout=None):
+    def _connect(self, host, port, timeout=None):
         addressInfoList = socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM)
         if not addressInfoList:
             raise ConnectionResolveError(host, port)
+        print(addressInfoList)
 
         start = time()
         errors = []
@@ -59,11 +77,15 @@ class AbstractService(object):
             try:
                 self._pipe = Pipe(sock)
                 remainingTimeout = timeout - (time() - start) if timeout is not None else None
-                yield self._pipe.connect(address, timeout=remainingTimeout)
+                yield self._pipe.connect(address, timeout=remainingTimeout, blocking=self.isBlocking)
             except ConnectionError as err:
                 errors.append(err)
             else:
-                self._configureStreams()
+                self._ioLoop = self._pipe._ioLoop
+                self._writableStream = WritableStream(self._ioLoop, self._pipe)
+                self._readableStream = ReadableStream(self._ioLoop, self._pipe)
+                self._ioLoop.bind_on_fd(self._pipe.fileno())
+                self._readableStream.bind(self.decoder.decode)
                 return
 
         if time() - start > timeout:
@@ -71,38 +93,6 @@ class AbstractService(object):
 
         reason = 'multiple connection errors: ' + ', '.join(err.message for err in errors)
         raise ConnectionError(host, port, reason)
-
-    def _connectSynchronously(self, host, port, timeout=None):
-        addressInfoList = socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM)
-        if not addressInfoList:
-            raise ConnectionResolveError(host, port)
-
-        start = time()
-        errors = []
-        for family, socktype, proto, canonname, address in addressInfoList:
-            sock = socket.socket(family=family, type=socktype, proto=proto)
-            try:
-                self._pipe = Pipe(sock)
-                remainingTimeout = timeout - (time() - start) if timeout is not None else None
-                self._pipe.connect(address, timeout=remainingTimeout, blocking=True)
-            except ConnectionError as err:
-                errors.append(err)
-            else:
-                self._configureStreams()
-                return
-
-        if time() - start > timeout:
-            raise ConnectionTimeoutError(host, port, timeout)
-
-        reason = 'multiple connection errors: ' + ', '.join(err.message for err in errors)
-        raise ConnectionError(host, port, reason)
-
-    def _configureStreams(self):
-        self._ioLoop = self._pipe._ioLoop
-        self._writableStream = WritableStream(self._ioLoop, self._pipe)
-        self._readableStream = ReadableStream(self._ioLoop, self._pipe)
-        self._ioLoop.bind_on_fd(self._pipe.fileno())
-        self._readableStream.bind(self.decoder.decode)
 
     def _on_message(self, args):
         msg = Message.initialize(args)
@@ -135,60 +125,53 @@ class AbstractService(object):
 
 
 class Locator(AbstractService):
-    def __init__(self, blocking):
-        super(Locator, self).__init__('locator', blocking)
+    def __init__(self, isBlocking):
+        super(Locator, self).__init__('locator', isBlocking)
 
     def connect(self, host, port):
         return self._connect(host, port)
 
     def resolve(self, name, timeout=None):
-        if not self.blocking:
-            return self.closure(RESOLVE_METHOD_ID)(name)
+        if self.isBlocking:
+            return self._blockingResolve(name, timeout)
         else:
-            try:
-                self._pipe.sock.settimeout(timeout)
-                self._writableStream.write([0, 1, [name]])
-                unpacker = msgpack.Unpacker()
-                messages = []
-                while True:
-                    response = self._pipe.sock.recv(4096)
-                    unpacker.feed(response)
-                    for msg in unpacker:
-                        msg = Message.initialize(msg)
-                        messages.append(msg)
-                    if messages and messages[-1].id == message.RPC_CHOKE:
-                        break
-                assert len(messages) == 2, 'protocol is corrupted! Locator must return exactly 2 chunks'
-                chunk, choke = messages
-                if chunk.id == message.RPC_ERROR:
-                    raise Exception(chunk.message)
-                return msgpack.loads(chunk.data)
-            finally:
-                self._pipe.sock.setblocking(False)
+            return self._nonBlockingResolve(name, timeout)
+
+    def _blockingResolve(self, name, timeout):
+        try:
+            self._pipe.sock.settimeout(timeout)
+            self._writableStream.write([0, 1, [name]])
+            unpacker = msgpack.Unpacker()
+            messages = []
+            while True:
+                response = self._pipe.sock.recv(4096)
+                unpacker.feed(response)
+                for msg in unpacker:
+                    msg = Message.initialize(msg)
+                    messages.append(msg)
+                if messages and messages[-1].id == message.RPC_CHOKE:
+                    break
+            assert len(messages) == 2, 'protocol is corrupted! Locator must return exactly 2 chunks'
+            chunk, choke = messages
+            if chunk.id == message.RPC_ERROR:
+                raise Exception(chunk.message)
+            return msgpack.loads(chunk.data)
+        finally:
+            self._pipe.sock.setblocking(False)
+
+    def _nonBlockingResolve(self, name, timeout):
+        return self.closure(RESOLVE_METHOD_ID)(name)
 
 
 class Service(AbstractService):
-    def __init__(self, name, blocking=False):
-        super(Service, self).__init__(name, blocking)
-        self.locator = Locator(blocking)
+    def __init__(self, name, isBlocking=False):
+        super(Service, self).__init__(name, isBlocking)
+        self.locator = Locator(isBlocking)
+        self.connect = strategy.init(self.connect, isBlocking)
 
     def connect(self, host='127.0.0.1', port=10053):
-        if self.blocking:
-            return self._resolveAndConnectSynchronously(host, port)
-        else:
-            return self._resolveAndConnectAsynchronously(host, port)
-
-    @chain.source
-    def _resolveAndConnectAsynchronously(self, host, port):
         yield self.locator.connect(host, port)
         endpoint, session, api = yield self.locator.resolve(self.name)
         yield self._connect(*endpoint)
-        for methodId, methodName in api.items():
-            setattr(self, methodName, self.closure(methodId))
-
-    def _resolveAndConnectSynchronously(self, host, port):
-        self.locator.connect(host, port)
-        endpoint, session, api = self.locator.resolve(self.name)
-        self._connect(*endpoint)
         for methodId, methodName in api.items():
             setattr(self, methodName, self.closure(methodId))
