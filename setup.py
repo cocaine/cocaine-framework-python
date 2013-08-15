628c0543cef5e14a8b9b621a702dc564efa6584a
Evgeny Safronov <division494@gmail.com>
2013-08-15 18:03:15 +0400

    Setup script review.
    * proxy installation is now optional
    * proxy will be installed if tools is installed
    * some additional fields added

--- setup.py
+++ setup.py
@@ -30,55 +30,75 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 try:
     with open("%s/debian/changelog" % BASE_DIR, 'r') as f:
         _version = re.findall(r"\(([^)]*)\)", f.readline())[0]
-        print("Find version %s in debian/changelog" % _version)
+        print("Found version %s in debian/changelog" % _version)
 except Exception:
     _version = "0.10.6.0"
 
+tools_requires = []
+tools_packages = []
+tools_scripts = []
+tools_data = []
 if "--without-tools" in sys.argv:
-    tools_requires = []
-    tools_data = []
-    tools_packages = []
     sys.argv.remove("--without-tools")
-    without_tools = True
 else:
-    without_tools = False
-    #os.system("python {0}/generate_man.py > {0}/manpages/cocaine-tool.5".format(BASE_DIR))
+    sys.argv.append('--with-proxy')
     tools_requires = ['opster >= 4.0']
     tools_packages = ["cocaine.tools", "cocaine.tools.helpers"]
+    tools_scripts = ["scripts/cocaine-tool"]
     if 'DEB_BUILD_GNU_TYPE' in os.environ:
         tools_data = [
             ('/etc/bash_completion.d/', ["scripts/bash_completion.d/cocaine-tool"])
         ]
 
+proxy_packages = []
+proxy_scripts = []
+proxy_data = []
+if '--with-proxy' in sys.argv:
+    sys.argv.remove('--with-proxy')
+    proxy_packages = ["cocaine.proxy"]
+    proxy_scripts = ["scripts/cocaine-tornado-proxy"]
+    proxy_data = [
+        ('/etc/cocaine/', ['init/cocaine-tornado-proxy.conf']),
+        ('/etc/init.d/', ['init/cocaine-tornado-proxy'])
+    ]
 
 setup(
     name="cocaine",
     version=_version,
-    description="Cocaine Python Framework and Tools for Cocaine Application Cloud",
-    long_description="A simple framework to ease the development of Cocaine apps\
-    and tools for deploying applications in the cloud",
-    url="https://github.com/cocaine/cocaine-framework-python",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
+    maintainer='Evgeny Safronov',
+    maintainer_email='division494@gmail.com',
+    url="https://github.com/cocaine/cocaine-framework-python",
+    description="Cocaine Python Framework and Tools for Cocaine Application Cloud.",
+    long_description=("A simple framework to ease the development of Cocaine apps "
+                      "and tools for deploying applications in the cloud"),
     license="LGPLv3+",
     platforms=["Linux", "BSD", "MacOS"],
-    packages=[
+    packages= [
         "cocaine",
         "cocaine.asio",
         "cocaine.decorators",
         "cocaine.services",
         "cocaine.futures",
         "cocaine.logging",
-        "cocaine.proxy",
         "cocaine.tools",
         "cocaine.tools.actions",
         "cocaine.testing",
-    ] + tools_packages,
+    ] + tools_packages + proxy_packages,
     install_requires=["msgpack_python", "tornado"] + tools_requires,
-    scripts=["scripts/cocaine-tornado-proxy", "scripts/cocaine-tool"] if not without_tools else [],
-    data_files=[('/etc/cocaine/', ['init/cocaine-tornado-proxy.conf']),
-                ('/etc/init.d/', ['init/cocaine-tornado-proxy'])],
+    scripts=tools_scripts + proxy_scripts,
+    data_files=proxy_data,
     tests_require=["mockito"],
-    test_suite="unittest.TestCase"
-)
+    test_suite="unittest.TestCase",
 
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+    ],
+)
