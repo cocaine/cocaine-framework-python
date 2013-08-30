1b67f9477b5e0b5cd59ac0b038827f76bdbc5cb9
Evgeny Safronov <division494@gmail.com>
2013-08-30 22:46:03 +0400

    Docker info implemented.
    * asynchronous, of course
    * some functional decomposition done.

--- cocaine/tools/actions/docker.py
+++ cocaine/tools/actions/docker.py
@@ -11,25 +11,55 @@ from cocaine.tools.helpers._unix import AsyncUnixHTTPClient
 __author__ = 'Evgeny Safronov <division494@gmail.com>'
 
 
-class Build(object):
-    def __init__(self, path, tag=None, quiet=False,
-                 streaming=None, timeout=120.0,
-                 url='unix://var/run/docker.sock', version='1.4', io_loop=None):
-        self._unix = url.startswith('unix://')
-        self._path = path
-        self._tag = tag
-        self._quiet = quiet
-        self._streaming = streaming
-        self._timeout = timeout
-        self._version = version
-        self._io_loop = io_loop or IOLoop.current()
+class Client(object):
+    def __init__(self, url='unix://var/run/docker.sock', version='1.4', io_loop=None):
+        self.url = url
+        self.version = version
+        self._io_loop = io_loop
 
+    def info(self):
+        return Info(self.url, self.version, self._io_loop).execute()
+
+    def build(self, path, tag=None, quiet=False, streaming=None, timeout=120.0):
+        return Build(path, tag, quiet, streaming, timeout, self.url, self.version, self._io_loop).execute()
+
+
+class Action(object):
+    def __init__(self, url, version, io_loop=None):
+        self._unix = url.startswith('unix://')
+        self._version = version
+        self._io_loop = io_loop
         if self._unix:
             self._base_url = url
             self._http_client = AsyncUnixHTTPClient(self._io_loop, url)
         else:
             self._base_url = '{0}/v{1}'.format(url, version)
             self._http_client = AsyncHTTPClient(self._io_loop)
+
+    def execute(self):
+        raise NotImplementedError
+
+    def _url(self, path):
+        return '{0}{1}'.format(self._base_url, path)
+
+
+class Info(Action):
+    @chain.source
+    def execute(self):
+        response = yield self._http_client.fetch(self._url('/info'))
+        yield response.body
+
+
+class Build(Action):
+    def __init__(self, path, tag=None, quiet=False, streaming=None, timeout=120.0, url='unix://var/run/docker.sock',
+                 version='1.4', io_loop=None):
+        super(Build, self).__init__(url, version, io_loop)
+        self._path = path
+        self._tag = tag
+        self._quiet = quiet
+        self._streaming = streaming
+        self._timeout = timeout
+        self._io_loop = io_loop or IOLoop.current()
 
     @chain.source
     def execute(self):
@@ -67,6 +97,3 @@ class Build(object):
             return stream.getvalue()
         finally:
             stream.close()
-
-    def _url(self, path):
-        return '{0}{1}'.format(self._base_url, path)
