265dbb289b808a8031e4acf34278520a3c81408a
Evgeny Safronov <division494@gmail.com>
2013-09-30 12:10:04 +0400

    Missing profile test for starting app added.

--- tests/test_tools_functional.py
+++ tests/test_tools_functional.py
@@ -271,3 +271,15 @@ class ToolsTestCase(unittest.TestCase):
                           'error in service "node" - '
                           'object \'test_app\' has not been found in \'manifests\': '
                           'object has not been found [2]\n'), err)
+
+    def test_app_start_fail_because_of_profile(self):
+        with upload_app('test_app'):
+            code, out, err = call([COCAINE_TOOL, 'app', 'start',
+                                   '--name', 'test_app',
+                                   '--profile', 'default'])
+            self.assertEqual(1, code)
+            self.assertEqual('', trim(out))
+            self.assertEqual(('Error occurred: '
+                              'error in service "node" - '
+                              'object \'default\' has not been found in \'profiles\': '
+                              'object has not been found [2]\n'), err)
