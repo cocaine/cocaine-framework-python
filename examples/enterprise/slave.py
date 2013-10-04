1b5f4f15cfde4a6ceb4a8fcfc3044df9ece08c04
Evgeny Safronov <division494@gmail.com>
2013-10-04 16:03:33 +0400

    Less external name dependencies for django example.

--- examples/enterprise/slave.py
+++ examples/enterprise/slave.py
@@ -8,6 +8,8 @@ from cocaine.worker import Worker
 
 __author__ = 'Evgeny Safronov <division494@gmail.com>'
 
+PROJECT_NAME = 'enterprise'
+
 
 log = logging.getLogger(__name__)
 cocaineHandler = LoggerHandler()
@@ -18,8 +20,8 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 worker = Worker()
 worker.run({
     'work': django(**{
-        'root': os.path.join(PROJECT_ROOT, 'enterprise'),
-        'settings': 'enterprise.settings',
+        'root': os.path.join(PROJECT_ROOT, PROJECT_NAME),
+        'settings': '{0}.settings'.format(PROJECT_NAME),
         'async': True,
         'log': log
     })
