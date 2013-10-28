305e1198a70e7a9560fdeaae31a9b5172bbb0855
Evgeny Safronov <division494@gmail.com>
2013-10-28 14:56:31 +0400

    PEP-8 style.

--- cocaine/testing/mocks.py
+++ cocaine/testing/mocks.py
@@ -1,10 +1,8 @@
 import datetime
-import sys
-
-from tornado.ioloop import IOLoop
-from cocaine.concurrent import Deferred
 
 from cocaine.exceptions import ChokeEvent
+from cocaine.concurrent import Deferred
+
 
 __author__ = 'Evgeny Safronov <division494@gmail.com>'
 
@@ -21,34 +19,34 @@ class CallableMock(object):
 
 
 class FutureTestMock(Deferred):
-        def __init__(self, ioLoop, chunks=None, interval=0.01):
-            super(FutureTestMock, self).__init__()
-            self.ioLoop = ioLoop
-            self.chunks = chunks
-            self.interval = interval
-            self.currentChunkId = 0
+    def __init__(self, ioLoop, chunks=None, interval=0.01):
+        super(FutureTestMock, self).__init__()
+        self.ioLoop = ioLoop
+        self.chunks = chunks
+        self.interval = interval
+        self.currentChunkId = 0
 
-        def bind(self, callback, errorback=None, on_done=None):
-            self.callback = callback
-            self.errorback = errorback
-            self.doneback = on_done
-            self.start()
+    def bind(self, callback, errorback=None, on_done=None):
+        self.callback = callback
+        self.errorback = errorback
+        self.doneback = on_done
+        self.start()
 
-        def start(self):
-            for pos, value in enumerate(self.chunks):
-                delta = datetime.timedelta(seconds=(pos + 1) * self.interval)
-                self.ioLoop.add_timeout(delta, self.invoke)
+    def start(self):
+        for pos, value in enumerate(self.chunks):
+            delta = datetime.timedelta(seconds=(pos + 1) * self.interval)
+            self.ioLoop.add_timeout(delta, self.invoke)
 
-            delta = datetime.timedelta(seconds=(len(self.chunks) + 1) * self.interval)
-            self.ioLoop.add_timeout(delta, self.choke)
+        delta = datetime.timedelta(seconds=(len(self.chunks) + 1) * self.interval)
+        self.ioLoop.add_timeout(delta, self.choke)
 
-        def invoke(self):
-            chunk = self.chunks[self.currentChunkId]
-            self.currentChunkId += 1
-            if isinstance(chunk, Exception):
-                self.errorback(chunk)
-            else:
-                self.callback(chunk)
+    def invoke(self):
+        chunk = self.chunks[self.currentChunkId]
+        self.currentChunkId += 1
+        if isinstance(chunk, Exception):
+            self.errorback(chunk)
+        else:
+            self.callback(chunk)
 
-        def choke(self):
-            self.errorback(ChokeEvent())
+    def choke(self):
+        self.errorback(ChokeEvent())
