9ab43ca5e27bbd0b4b40954838abb4b417ee15d6
Anton Tyurin <noxiouz@yandex.ru>
2014-01-27 15:46:10 +0400

    Fix handshake and heartbeat timer logic

--- cocaine/worker.py
+++ cocaine/worker.py
@@ -53,7 +53,6 @@ class Worker(object):
 
         self.disown_timer = ev.Timer(self.on_disown, disown_timeout, self.loop)
         self.heartbeat_timer = ev.Timer(self.on_heartbeat, heartbeat_timeout, self.loop)
-        self.heartbeat_timer.start()
 
         if isinstance(self.endpoint, types.TupleType) or isinstance(self.endpoint, types.ListType):
             if len(self.endpoint) == 2:
@@ -81,10 +80,6 @@ class Worker(object):
         self.loop.register_read_event(self.r_stream._on_event,
                                       self.pipe.fileno())
         self._logger.debug("Worker with %s send handshake" % self.id)
-        # Send both messages - to run timers properly. This messages will be sent
-        # only after all initialization, so they have same purpose.
-        self._send_handshake()
-        self._send_heartbeat()
 
     def _init_endpoint(self, init_args):
         try:
@@ -100,6 +95,12 @@ class Worker(object):
             binds = {}
         for event, name in binds.iteritems():
             self.on(event, name)
+        # Run successfully
+        self._send_handshake()
+        # Ready to work
+        self._send_heartbeat()
+
+        self.heartbeat_timer.start()
         self.loop.run()
 
     def terminate(self, reason, msg):
