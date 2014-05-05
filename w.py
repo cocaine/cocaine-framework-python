3487c2037af5adbee2e82273fc1cf3081c1b977c
Anton Tyurin <noxiouz@yandex.ru>
2014-05-05 16:13:48 +0700

    update test worker code

--- w.py
+++ w.py
@@ -3,10 +3,13 @@
 import asyncio
 
 from cocaine.worker import Worker
+from cocaine.services import Service
 
 
 w = Worker(app="app", uuid="a", endpoint="enp",
            heartbeat_timeout=2, disown_timeout=1)
+
+node = Service("node")
 
 
 @asyncio.coroutine
@@ -14,7 +17,10 @@ def echo(request, response):
     yield asyncio.sleep(1)
     inp = yield request.read(timeout=1)
     print inp
-    response.write(inp)
+    fut = yield node.list()
+    result = yield fut.get()
+    print result
+    response.write(result)
     response.close()
 
 w.on("echo", echo)
