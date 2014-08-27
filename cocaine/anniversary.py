ee60d78971cce860157f4be74ff224c458c1ad03
Anton Tyurin <noxiouz@yandex.ru>
2014-08-28 00:50:35 +0400

    Just stole 1000th commit [skip ci]

---
+++ cocaine/anniversary.py
@@ -0,0 +1,55 @@
+#
+#    Copyright (c) 2012+ Anton Tyurin <noxiouz@yandex.ru>
+#    Copyright (c) 2013+ Evgeny Safronov <division494@gmail.com>
+#    Copyright (c) 2011-2014 Other contributors as noted in the AUTHORS file.
+#
+#    This file is part of Cocaine.
+#
+#    Cocaine is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Cocaine is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+ # __      __                      __
+# |  \    /  \                    |  \
+#  \$$\  /  $$  ______   __    __ | $$
+#   \$$\/  $$  |      \ |  \  |  \| $$
+#    \$$  $$    \$$$$$$\| $$  | $$| $$
+#     \$$$$    /      $$| $$  | $$ \$$
+#     | $$    |  $$$$$$$| $$__/ $$ __
+#     | $$     \$$    $$ \$$    $$|  \
+#      \$$      \$$$$$$$ _\$$$$$$$ \$$
+#                       |  \__| $$
+#                        \$$    $$
+#                         \$$$$$$
+#    __     ______    ______    ______     __      __
+#  _/  \   /      \  /      \  /      \   |  \    |  \
+# |   $$  |  $$$$$$\|  $$$$$$\|  $$$$$$\ _| $$_   | $$____
+#  \$$$$  | $$$\| $$| $$$\| $$| $$$\| $$|   $$ \  | $$    \
+#   | $$  | $$$$\ $$| $$$$\ $$| $$$$\ $$ \$$$$$$  | $$$$$$$\
+#   | $$  | $$\$$\$$| $$\$$\$$| $$\$$\$$  | $$ __ | $$  | $$
+#  _| $$_ | $$_\$$$$| $$_\$$$$| $$_\$$$$  | $$|  \| $$  | $$
+# |   $$ \ \$$  \$$$ \$$  \$$$ \$$  \$$$   \$$  $$| $$  | $$
+#  \$$$$$$  \$$$$$$   \$$$$$$   \$$$$$$     \$$$$  \$$   \$$
+
+
+
+#                                                  __    __      __
+#                                                 |  \  |  \    |  \
+#   _______   ______   ______ ____   ______ ____   \$$ _| $$_   | $$
+#  /       \ /      \ |      \    \ |      \    \ |  \|   $$ \  | $$
+# |  $$$$$$$|  $$$$$$\| $$$$$$\$$$$\| $$$$$$\$$$$\| $$ \$$$$$$  | $$
+# | $$      | $$  | $$| $$ | $$ | $$| $$ | $$ | $$| $$  | $$ __  \$$
+# | $$_____ | $$__/ $$| $$ | $$ | $$| $$ | $$ | $$| $$  | $$|  \ __
+#  \$$     \ \$$    $$| $$ | $$ | $$| $$ | $$ | $$| $$   \$$  $$|  \
+#   \$$$$$$$  \$$$$$$  \$$  \$$  \$$ \$$  \$$  \$$ \$$    \$$$$  \$$
