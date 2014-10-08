dc596917df7d8ac42d9b5370d59cd1b5e20d60d1
Anton Tiurin <noxiouz@yandex.ru>
2014-10-08 23:57:22 +0400

    [Examples] Added echo exmple

---
+++ examples/echo/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#    Copyright (c) 2014 Anton Tyurin <noxiouz@yandex.ru>
+#    This file is part of Cocaine.
+#
+#    Cocaine is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Cocaine is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from cocaine.worker import Worker
+
+
+def echo(request, response):
+    inc = yield request.read()
+    response.write(str(inc))
+    response.close()
+
+
+def main():
+    w = Worker()
+    w.on("ping", echo)
+    w.run()
+
+
+if __name__ == '__main__':
+    main()
