492f3735aed0516d782c3b05d051249c43a92f49
Anton Tiurin <noxiouz@yandex.ru>
2014-11-12 15:22:43 +0300

    Add comment about self.sock in Pipe.close

--- cocaine/asio/pipe.py
+++ cocaine/asio/pipe.py
@@ -98,7 +98,10 @@ class Pipe(object):
         self.address = None
         self._ioLoop.stop_listening(self.sock.fileno())
         self.sock.close()
-        # Seems there's no point
+        # To detach that sock fron eventloop
+        # we must know fd. If we set sock to None
+        # there's no way to find fd out.
+        # Also there's no place where None is checked.
         # self.sock = None
 
     def _onConnectionTimeout(self, address):
