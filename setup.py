9afad46dda69319e5aab41f74e944c4059dff5ea
Anton Tyurin <noxiouz@yandex.ru>
2014-12-16 18:00:32 +0400

    [Version] Fixed deps and bumped version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name="cocaine",
-    version="0.12.0.1-pre-rc3",
+    version="0.12.0.1-pre-rc4",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
