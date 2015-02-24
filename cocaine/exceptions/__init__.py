4c5a2620754f1399897710436e35f36b0f92e3f3
Anton Tyurin <noxiouz@yandex-team.ru>
2015-02-24 23:12:55 +0300

    [Travis] Switch on notifications again and fix PEP8 warnings.

--- cocaine/exceptions/__init__.py
+++ cocaine/exceptions/__init__.py
@@ -20,9 +20,9 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-__author__ = 'Evgeny Safronov <division494@gmail.com>'
-
 from ..common import CocaineErrno
+
+__author__ = 'Evgeny Safronov <division494@gmail.com>'
 
 
 class CocaineError(Exception):
