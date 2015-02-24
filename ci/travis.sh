5eb74875f732b16b79f14cfe6276ca7d943aabfe
Anton Tyurin <noxiouz@yandex-team.ru>
2015-02-24 22:32:13 +0300

    [Travis] Create env for tests

---
+++ ci/travis.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+install_utility () {
+    sudo apt-get update -qq && sudo apt-get install -qq devscripts build-essential equivs python-software-properties
+}
+
+build_cocaine () {
+  git clone --recursive https://github.com/cocaine/cocaine-core.git -b master
+  cd cocaine-core
+  yes | sudo mk-build-deps -i
+  yes | debuild -uc -us -j8
+  cd .. && sudo dpkg -i *.deb || sudo apt-get install -f && rm -rf cocaine-core 
+}
+
+make_env () {
+    echo "Install utility packages..."
+    install_utility
+    echo "Build & install packages..."
+    build_cocaine
+    echo "Waiting..."
+    sleep(5)
+}
+
+make_env
