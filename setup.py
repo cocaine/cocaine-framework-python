180c00b338778e35766c713fc47fb35ee3d3d4b3
Anton Tiurin <noxiouz@yandex.ru>
2015-06-05 18:39:48 +0300

    Version bump rc20

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name="cocaine",
-    version="0.12.0.1rc19",
+    version="0.12.0.1rc20",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
