52f56257c74f773199675db78b610fe7507cd1bd
Anton Tiurin <noxiouz@yandex.ru>
2015-10-07 17:01:55 +0300

    Version 0.12.4.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name="cocaine",
-    version="0.12.4.5",
+    version="0.12.4.6",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
