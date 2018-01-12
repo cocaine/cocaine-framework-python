6f4ae5ac3b38341c95f25e31dab29a01f8c746d7
Evgeny Safronov <division494@gmail.com>
2018-01-12 18:22:47 +0300

    version: bump - 0.12.14.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cocaine",
-    version="0.12.14.1",
+    version="0.12.14.2",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
