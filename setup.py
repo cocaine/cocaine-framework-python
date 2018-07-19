d8a30074b6338bac4389eb996e00d404338115e4
Хасанов Булат <afti@yandex.ru>
2018-07-19 14:15:27 +0300

    fix: update pip, wheel, setuptools versions in travis build (#82)

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
     test_suite='nose.collector',
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
--- tests/test_main.py
+++ tests/test_main.py
@@ -20,7 +20,6 @@
 #
 
 import logging
-import socket
 
 from tornado import gen
 from tornado.ioloop import IOLoop
